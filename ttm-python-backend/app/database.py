6eb6001fd32f58dc33a176522187d712e3820f80
imboster80 <imboster80@gmail.com>
2026-08-10 21:09:49 +0630

    Update database.py

--- ttm-python-backend/app/database.py
+++ ttm-python-backend/app/database.py
@@ -7,4 +7,5 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI") or os.getenv("MONGODB_URI") or "mongodb://localhost:27017/ttm_db"
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client.get_database()
+# Database နာမည်ကို တိုက်ရိုက်သတ်မှတ်ပေးခြင်း
+db = client.get_database("ttm_db")
